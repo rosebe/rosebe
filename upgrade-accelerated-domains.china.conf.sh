74692e4f2e313d8f55d0db7af5f77a710573bb7e
珐琅彩山水佩奇童车游春马蹄杯 <idgbook@gmail.com>
2018-10-10 15:46:05 +0800

    Update upgrade-accelerated-domains.china.conf.sh

--- upgrade-accelerated-domains.china.conf.sh
+++ upgrade-accelerated-domains.china.conf.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
-
-
-
 wget -c --no-check-certificate https://github.com/felixonmars/dnsmasq-china-list/raw/master/accelerated-domains.china.conf \
-     -O /etc/accelerated-domains.china-$(date "+%Y_%m_%d_%H:%M:%S").conf
+     -O /root/accelerated-domains.china-$(date "+%Y_%m_%d_%H:%M:%S").conf
