cf37987e7975edfcf3dcb272ee8b2f5869cf3251
珐琅彩山水佩奇童车游春马蹄杯 <idgbook@gmail.com>
2018-12-12 10:16:36 +0800

    Update update-geosite.dat.sh

--- update-geosite.dat.sh
+++ update-geosite.dat.sh
@@ -1,4 +1,4 @@
-# Update geosite.dat
+# Update geosite.dat  release/release-ci.sh
 
 GEOIP_TAG=$(curl --silent "https://api.github.com/repos/v2ray/domain-list-community/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
 curl -L -o release/config/geosite.dat "https://github.com/v2ray/domain-list-community/releases/download/${GEOIP_TAG}/dlc.dat"
