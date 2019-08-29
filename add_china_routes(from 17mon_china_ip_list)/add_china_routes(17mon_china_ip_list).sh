c27af8ba9a40b9a4ae0149b75fb89620ad2b6821
珐琅彩山水佩奇童车游春马蹄杯 <idgbook@gmail.com>
2019-08-29 12:08:43 +0800

    Update add_china_routes(17mon_china_ip_list).sh

--- add_china_routes(from 17mon_china_ip_list)/add_china_routes(17mon_china_ip_list).sh
+++ add_china_routes(from 17mon_china_ip_list)/add_china_routes(17mon_china_ip_list).sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+curl -L -H "Cache-Control: no-cache" -O https://raw.githubusercontent.com/17mon/china_ip_list/master/china_ip_list.txt
 
 echo create china_routes hash:net family inet hashsize 2048 maxelem 9999 >> ipset-save-china-router.list
 
